1d92c4e7937d1807cd0bad06dce71ac7b996ddea
Victor Lourenço <victor.lourenco@tgid.com.br>
2022-12-27 14:48:18 -0300

    FEAT: Testantando a conexao com o banco excluindo e recuperando objetos da base de dados

--- jpa.maven/src/main/java/aplicacao/Programa.java
+++ jpa.maven/src/main/java/aplicacao/Programa.java
@@ -1,19 +1,43 @@
 package aplicacao;
 
 import dominio.Pessoa;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class Programa {
 	
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Victor Lourenço", "victor@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Iolanda da Silva", "iolanda@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Marly de Souza", "marly@gmail.com");
+//		Pessoa p1 = new Pessoa(null, "Victor Lourenço", "victor@gmail.com");
+//		Pessoa p2 = new Pessoa(null, "Iolanda da Silva", "iolanda@gmail.com");
+//		Pessoa p3 = new Pessoa(null, "Marly de Souza", "marly@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+//		em.getTransaction().begin();
+//		em.persist(p1);
+//		em.persist(p2);
+//		em.persist(p3);
+//		em.getTransaction().commit();
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		
+		System.out.println("Pessoa excluida!");
+		
+		em.close();
+		emf.close();
 		
 	}
+	
+	// PS: Um objeto monitorado é um objeto que acabou der ser inserido e a 
+	// conexão ainda não foi fechada,  ou é um objeto que foi recuperado do banco.
+	// Ex: em.find(obj);
 
 }
--- jpa.maven/src/main/java/dominio/Pessoa.java
+++ jpa.maven/src/main/java/dominio/Pessoa.java
@@ -2,10 +2,18 @@ package dominio;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Pessoa implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String email;
