fc029174a9084bc332e0cf73b802d8a48c49d4f5
Victor Lourenço <victor.lourenco@tgid.com.br>
2022-12-28 14:41:16 -0300

    FEAT: Criando operacao de instanciacao com a interface CommandLineRunner e a classe CustomerRepository

--- vendas/src/main/java/com/victor/vendas/Application.java
+++ vendas/src/main/java/com/victor/vendas/Application.java
@@ -1,13 +1,33 @@
 package com.victor.vendas;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.victor.vendas.domain.Categoria;
+import com.victor.vendas.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Setup");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 
 }
