6781518c793f535756b62fdd46d6509cb8d995e2
Victor Lourenço <victor.lourenco@tgid.com.br>
2022-12-28 13:30:35 -0300

    TEST: Testando o REST da aplicacao atraves do endpoint CategoriaResource

---
+++ vendas/src/main/java/com/victor/vendas/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.victor.vendas.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String menssagem() {
+		return "REST está funcionando!";
+	}
+
+}
