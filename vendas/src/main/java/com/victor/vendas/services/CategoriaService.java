762192060e8ffd8473e2d89ea1509370f5120d05
Victor Lourenço <victor.lourenco@tgid.com.br>
2022-12-30 18:01:02 -0300

    FEAT: Efetuando o tratamento de exceção no endpoit categoria/{id}

--- vendas/src/main/java/com/victor/vendas/services/CategoriaService.java
+++ vendas/src/main/java/com/victor/vendas/services/CategoriaService.java
@@ -7,16 +7,19 @@ import org.springframework.stereotype.Service;
 
 import com.victor.vendas.domain.Categoria;
 import com.victor.vendas.repositories.CategoriaRepository;
+import com.victor.vendas.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+
+		return obj.orElseThrow(
+				() -> new ObjectNotFoundException("Objeto com id " + id + " não encontrado na base de dados, Tipo: " + Categoria.class.getName()));
 	}
-	
+
 }
