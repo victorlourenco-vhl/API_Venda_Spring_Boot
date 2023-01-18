9f141582e2bf5b683cde251ca764cde812a5ccad
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-01-18 14:15:16 -0300

    FEAT: Implementando o metodo delete em categoria e criando excecao personalizada para integridade de dados

--- vendas/src/main/java/com/victor/vendas/services/CategoriaService.java
+++ vendas/src/main/java/com/victor/vendas/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.victor.vendas.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.victor.vendas.domain.Categoria;
 import com.victor.vendas.repositories.CategoriaRepository;
+import com.victor.vendas.services.exceptions.DataIntegrityException;
 import com.victor.vendas.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,18 +20,27 @@ public class CategoriaService {
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 
-		return obj.orElseThrow(
-				() -> new ObjectNotFoundException("Objeto com id " + id + " não encontrado na base de dados, Tipo: " + Categoria.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto com id " + id + " não encontrado na base de dados, Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		findById(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produto");
+		}
 	}
 
 }
